3512687de8848cf3efa92a9156dddb5944dac392
David Steiman <d.steiman@ktel.de>
2018-01-26 15:33:11 +0100

    publish github local install script

---
+++ install-docker-kubeadm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "
+Package: docker-ce
+Pin: version 17.03.*
+Pin-Priority: 1000
+" > /etc/apt/preferences.d/docker-ce
+
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+apt-get update && apt-get install -y docker-ce
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
