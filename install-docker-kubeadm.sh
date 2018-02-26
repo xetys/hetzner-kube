691d1c4bcd3a1722aad969e4b2341b3f03f28821
David Steiman <d.steiman@stytex.de>
2018-02-26 14:09:08 +0100

    add wireguard and optimize install script [ci skip]

--- install-docker-kubeadm.sh
+++ install-docker-kubeadm.sh
@@ -8,24 +8,31 @@ Pin-Priority: 1000
 " > /etc/apt/preferences.d/docker-ce
 
 apt-get update
+# transport stuff
 apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
+
+# docker-ce
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
    $(lsb_release -cs) \
    stable"
-apt-get update && apt-get install -y docker-ce
+# kubernetes
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+
+# prepare wireguard
+add-apt-repository ppa:wireguard/wireguard -y
+
+apt-get update && apt-get install -y docker-ce
+apt-get install -y docker-ce kubelet kubeadm kubectl wireguard
 
 # prepare for hetzners cloud controller manager
 mkdir -p /etc/systemd/system/kubelet.service.d
@@ -35,7 +42,6 @@ Environment="KUBELET_EXTRA_ARGS=--cloud-provider=external"
 EOM
 
 systemctl daemon-reload
-
 }
 
 S=$(type -p kubeadm > /dev/null &> /dev/null; echo $?)
