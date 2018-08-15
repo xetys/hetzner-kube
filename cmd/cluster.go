d32635187ecab3a68930f2a9c7465cd019bc6733
Marco Vito Moscaritolo <mavimo@gmail.com>
2018-08-15 19:26:00 +0200

    code cleanup

--- cmd/cluster.go
+++ cmd/cluster.go
@@ -33,14 +33,4 @@ are hopefully coming soon.'`,
 
 func init() {
 	rootCmd.AddCommand(clusterCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// clusterCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// clusterCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
